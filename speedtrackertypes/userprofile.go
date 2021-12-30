b60f9c23d0fe933fc24ff290769559dbbd4bc732
RedmondK <keithredmond@gmail.com>
2021-12-30 13:11:45 +0000

    Fixed issues in UserProfile

--- speedtrackertypes/userprofile.go
+++ speedtrackertypes/userprofile.go
@@ -1,7 +1,7 @@
 package speedtrackertypes
 
 type UserProfile struct {
-	EmailAddress string `json:"emailAddress"`,
-	FirstName string `json:"FirstName"`,
-	LastName string `json:LastName`
+	EmailAddress string `json:"emailAddress"`
+	FirstName    string `json:"firstName"`
+	LastName     string `json:"lastName"`
 }
