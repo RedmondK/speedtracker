b5740456a667d79524ea8ea8716af381183607c7
RedmondK <keithredmond@gmail.com>
2022-01-11 01:09:36 +0000

    Refactor of PBData Update completed

--- speedtrackertypes/personalbesthelper.go
+++ speedtrackertypes/personalbesthelper.go
@@ -2,60 +2,6 @@ package speedtrackertypes
 
 import "time"
 
-func GetUpdatedPersonalBestsUsingNewSwings(sessionDate time.Time, personalBests []PersonalBest, newSwings []Swing) (newPersonalBests []PersonalBest, createdPBs []PersonalBest) {
-	for i := 0; i < len(newSwings); i++ {
-		swing := newSwings[i]
-		pbFound := false
-
-		pb := PersonalBest{}
-
-		for j := 0; j < len(personalBests); j++ {
-			pb = personalBests[j]
-			if swing.Colour == pb.Swing.Colour && swing.Position == pb.Swing.Position && swing.Side == pb.Swing.Side {
-				pbFound = true
-
-				if swing.Speed > pb.Swing.Speed {
-					personalBests[j].Date = sessionDate
-					personalBests[j].Swing = swing
-
-					newPB := PersonalBest{Date: sessionDate, Swing: swing}
-					createdPBs = append(createdPBs, newPB)
-					break
-				}
-			}
-		}
-
-		if !pbFound {
-			newPB := PersonalBest{Date: sessionDate, Swing: swing}
-			createdPBs = append(createdPBs, newPB)
-			personalBests = append(personalBests, newPB)
-		}
-	}
-
-	return personalBests, createdPBs
-}
-
-func GetObsoletePBHistoryWithNewPBs(pbNotInHistory PersonalBest, existingPBHistory []PersonalBestHistoryRecord) (obsoleteHistoryRecords []PersonalBestHistoryRecord) {
-	for i := 0; i < len(existingPBHistory); i++ {
-		historyPBDate := existingPBHistory[i].PersonalBest.Date
-		historyPBSwing := existingPBHistory[i].PersonalBest.Swing
-
-		if historyPBDate.Before(pbNotInHistory.Date) { //older pbs cannot be made obsolete by a faster one
-			continue
-		}
-
-		if historyPBSwing.Colour != pbNotInHistory.Swing.Colour || historyPBSwing.Position != pbNotInHistory.Swing.Position || historyPBSwing.Side != pbNotInHistory.Swing.Side {
-			continue
-		}
-
-		if existingPBHistory[i].Speed < pbNotInHistory.Swing.Speed { //a newer pb which is actually slower than this one is obsolete
-			obsoleteHistoryRecords = append(obsoleteHistoryRecords, existingPBHistory[i])
-		}
-	}
-
-	return obsoleteHistoryRecords
-}
-
 func GetUpdatedPersonalBestData(sessionDate time.Time, personalBests []PersonalBest, newSwings []Swing, existingPBHistory []PersonalBestHistoryRecord) (newPersonalBests []PersonalBest, createdPBs []PersonalBest, obsoleteHistoryRecords []PersonalBestHistoryRecord) {
 	for i := 0; i < len(newSwings); i++ {
 		swing := newSwings[i]
